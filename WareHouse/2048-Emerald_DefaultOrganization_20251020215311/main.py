f38cac8040b042e954c54fcfe8f681cb99654b21
emeraldsingh <singhe@warhawks.ulm.edu>
2025-11-13 16:20:24 -0500

    changes

--- WareHouse/2048-Emerald_DefaultOrganization_20251020215311/main.py
+++
@@ -1,6 +0,0 @@
-'''
-Main entry point for the 2048 game application
-'''
-from game_gui import main
-if __name__ == "__main__":
-    main()
--- WareHouse/Chess-Emerald_DefaultOrganization_20251008213610/player.py
+++
@@ -1,6 +0,0 @@
-'''
-Player class representing each player in the chess game.
-'''
-class Player:
-    def __init__(self, color):
-        self.color = color
